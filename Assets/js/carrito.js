269c903393484756ecc8f3a282c68dac1a942b7b
AriasSacigaBryanDavid <bariassaciga@gmail.com>
2022-07-10 01:43:20 -0500

    Video 12: Agregar productos al carrito

--- Assets/js/carrito.js
+++ Assets/js/carrito.js
@@ -1,19 +1,33 @@
 const btnAddDeseo = document.querySelectorAll('.btnAddDeseo');
+const btnAddCarrito = document.querySelectorAll('.btnAddCarrito');
 const btnDeseo = document.querySelector('#btnCantidadDeseo');
-let listaDeseo;
+const btnCarrito = document.querySelector('#btnCantidadCarrito');
+
+let listaDeseo, listaCarrito;
 document.addEventListener('DOMContentLoaded', function(){
     if (localStorage.getItem('listaDeseo') != null) {
         listaDeseo = JSON.parse(localStorage.getItem('listaDeseo'));
     }
-    cantidadDeseo();
-    for (let i = 0; i<btnAddDeseo.length; i++) {
-      btnAddDeseo[i].addEventListener('click', function() {
-        let idProducto = btnAddDeseo[i].getAttribute('prod');
-        agregarDeseo(idProducto);
-      })   
+    if (localStorage.getItem('listaCarrito') != null) {
+        listaCarrito = JSON.parse(localStorage.getItem('listaCarrito'));
     }
+    
+    for (let i = 0; i<btnAddDeseo.length; i++) {
+        btnAddDeseo[i].addEventListener('click', function() {
+            let idProducto = btnAddDeseo[i].getAttribute('prod');
+            agregarDeseo(idProducto);
+        })   
+    }
+    for (let i = 0; i<btnAddCarrito.length; i++) {
+        btnAddCarrito[i].addEventListener('click', function() {
+            let idProducto = btnAddCarrito[i].getAttribute('prod');
+            agregarCarrito(idProducto,1);
+          })
+    }
+    cantidadDeseo();
+    cantidadCarrito();
 })
-
+//Agregar productos a la lista de deseo
 function agregarDeseo(idProducto) {
     if (localStorage.getItem('listaDeseo') == null) {
         listaDeseo = [];
@@ -49,5 +63,44 @@ function cantidadDeseo() {
         btnDeseo.textContent = listas.length;        
     } else {
         btnDeseo.textContent = 0;          
+    }   
+}
+
+//Agregar productos al Carrito
+function agregarCarrito(idProducto, cantidad) {
+    if (localStorage.getItem('listaCarrito') == null) {
+        listaCarrito = [];
+    } else {
+        let listaExiste = JSON.parse(localStorage.getItem('listaCarrito'));
+        for (let i = 0; i < listaExiste.length; i++) {
+           if (listaExiste[i]['idProducto'] == idProducto) {
+                Swal.fire(
+                'Aviso',
+                'EL PRODUCTO YA ESTA agregado',
+                'warning'
+                )
+                return;
+           }
+        }
+        listaCarrito.concat(localStorage.getItem('listaCarrito'));
+    }
+    listaCarrito.push({
+        "idProducto": idProducto,
+        "cantidad": cantidad
+    })
+    localStorage.setItem('listaCarrito', JSON.stringify(listaCarrito));
+    Swal.fire(
+        'Aviso',
+        'PRODUCTO AGREGADO AL CARRITO',
+        'success'
+      )
+    cantidadCarrito();
+}
+function cantidadCarrito() {
+    let listas = JSON.parse(localStorage.getItem('listaCarrito'));
+    if (listas != null) {
+        btnCarrito.textContent = listas.length;        
+    } else {
+        btnCarrito.textContent = 0;          
     }   
 }
